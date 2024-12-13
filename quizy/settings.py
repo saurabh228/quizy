0189e52c7b34726c5c3ae0518dbf3ed6e4f58952
Saurabh Saini <saurabhsaini228@gmail.com>
2024-12-14 01:55:32 +0530

    use sqlite for local

--- quizy/settings.py
+++ quizy/settings.py
@@ -63,16 +63,16 @@ WSGI_APPLICATION = 'quizy.wsgi.application'
 
 # Database
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
 
 # Password validation
 
